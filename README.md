c097227daf11089bd0a16b68a9e3ddeaa6ce921c
Jose Antonio <josew383@gmail.com>
2017-12-24 01:34:02 +0100

    New funtions added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # YeelightPro
+Easy project in python to learn the basics of yeelight RGB
--- YeelightPro.py
+++ YeelightPro.py
@@ -4,17 +4,15 @@ import socket
 from time import sleep
 
 def operate_on_bulb(ip, method, params):
-
-  bulb_ip='192.168.4.234'
   port=55443
   try:
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print "Conectando con ",bulb_ip, port ,"..."
-    tcp_socket.connect((bulb_ip, int(port)))
+    print "Conectando con ",ip, port ,"..."
+    tcp_socket.connect((ip, int(port)))
 
-    msg2="{\"id\": 192.168.4.234, \"method\": \"set_rgb\", \"params\":[\"65280\", \"smooth\", 500]}\r\n"
+    msg2="{\"id\": 192.168.4.234, \"method\": \"set_rgb\", \"params\":[\"65280\", \"sudden\", 500]}\r\n"
 
-    msg="{\"id\":" + str(bulb_ip) + ",\"method\":\""
+    msg="{\"id\":" + str(ip) + ",\"method\":\""
     msg += method + "\",\"params\":[" + params + "]}\r\n"
 
     tcp_socket.send(msg)
@@ -23,17 +21,30 @@ def operate_on_bulb(ip, method, params):
     print "Ha habido un error:", e
 
 
-def set_rgb(idx, color):
-  operate_on_bulb(idx, "set_rgb", str(color))
+def set_rgb(ip, color):
+    operate_on_bulb(ip, "set_rgb", str(color))
 
-def set_bright(idx, bright):
-  operate_on_bulb(idx, "set_bright", str(bright))
+def set_bright(ip, bright):
+    operate_on_bulb(ip, "set_bright", str(bright))
+    #effect (str)  The type of effect. Can be "smooth" or "sudden".
 
+def set_color_temp(ip):
+    operate_on_bulb(ip,"set_color_temp","")
+    #Parameters:	degrees (int)  The degrees to set the color temperature to (1700 _ 6500).
+
+def toggle(ip):
+    operate_on_bulb(ip,"toggle","")
+
+def turn_on(ip):
+    operate_on_bulb(ip,"turn_on","")
+
+def turn_off(ip):
+    operate_on_bulb(ip,"turn_off","")
 
 #MAIN DEL PROGRAMA DE LA BOMBILLA
 print "Biemvenido al programa de YEELIGHT"
-print "Cargando configuracion..."
 
+turn_on('192.168.4.234')
 set_rgb('192.168.4.234', 16777215)
 
 set_bright('192.168.4.234', 100)
@@ -45,4 +56,5 @@ sleep(0.5)
 set_bright('192.168.4.234', 1)
 sleep(0.5)
 set_bright('192.168.4.234', 100)
-input("Pressione ENTER para continuar")
+
+#input("Pressione ENTER para continuar") #For view log
