b1a1256c01466e92ec7ea0b7e0075895db2175ea
Jose Antonio <josew383@gmail.com>
2017-12-28 17:46:34 +0100

    Minor Change

--- YeelightPro.py
+++ YeelightPro.py
@@ -33,11 +33,11 @@ def operate_on_bulb(ip, method, params):
 
 def set_rgb(ip, color):
     #white 16777215 blue 255 green 65280 red 16711680 pink 16711935 yellow 16776960 turquoise 65535
-    params=",\"sudden\",500"
+    params=",\"smooth\",500"
     operate_on_bulb(ip, "set_rgb", str(color)+params)
 
 def set_bright(ip, bright):
-    params=",\"sudden\",500"
+    params=",\"smooth\",500"
     operate_on_bulb(ip, "set_bright", str(bright)+params)
     #effect (str)  The type of effect. Can be "smooth" or "sudden".
     #Minimun of bright is 1!!
@@ -50,11 +50,11 @@ def toggle(ip):
     operate_on_bulb(ip,"toggle","")
 
 def turn_on(ip):
-    params="\"on\",\"sudden\",500"
+    params="\"on\",\"smooth\",500"
     operate_on_bulb(ip,"set_power",params)
 
 def turn_off(ip):
-    params="\"off\",\"sudden\",500"
+    params="\"off\",\"smooth\",500"
     operate_on_bulb(ip,"set_power",params)
 
 #-------------------------------------------------------------------------
